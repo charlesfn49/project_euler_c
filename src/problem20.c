ee276956c9291dbdb2582e701a1a89e4eefe15f1
RobertMeade <meade.develop3r@gmail.com>
2016-04-04 02:31:46 -0400

    Added header comment problem20

--- src/problem20.c
+++ src/problem20.c
@@ -2,6 +2,18 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+/**
+ * Project Euler Problem 20:
+ *   n! means n × (n − 1) × ... × 3 × 2 × 1
+ *
+ *   For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+ *   and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+ *
+ *   Find the sum of the digits in the number 100!
+ *
+ *   Answer: 648
+ */
+
 // sums the data of an int array
 int sum_array(int* arr)
 {
