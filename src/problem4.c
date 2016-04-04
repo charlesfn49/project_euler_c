1b03f7cf695eebf104923a444d884f30ba994dae
RobertMeade <meade.develop3r@gmail.com>
2016-04-03 20:57:35 -0400

    Directory refactor part 2; breaks compilation

--- src/problem4.c
+++ src/problem4.c
@@ -20,16 +20,21 @@ bool isPalindrome(unsigned int n)
 unsigned long max_palindrome_product(unsigned int max_digits)
 {
   unsigned long max_product = 0;
+  unsigned short sqrt_product = 0;
+
   unsigned long max_value = pow(10, max_digits); // stash max_value to avoid 10^max_digits calls to pow
   for(unsigned int i = max_value - 1; i > 0; i--)
   {
     for(unsigned int j = i - 1; j > 1; j--)
     {
-      unsigned short sqrt_product = sqrt(max_product); // stash sqrt to avoid calling library function twice
       unsigned int product = i * j; // stash product to avoid 3x multiplication
       
       // check if the number is the next largest palindrome
-      if(isPalindrome(product) && product > max_product) max_product = product;
+      if(product > max_product && isPalindrome(product)) 
+      {
+	max_product = product;
+	sqrt_product = sqrt(max_product);
+      }
       // if it is impossible for future products to be greater, return
       else if(i < sqrt_product && j < sqrt_product) return max_product;
     }
@@ -39,5 +44,5 @@ unsigned long max_palindrome_product(unsigned int max_digits)
 
 int main()
 {
-  printf("%lu\n", max_palindrome_product(5));
+  printf("%lu\n", max_palindrome_product(3));
 }
